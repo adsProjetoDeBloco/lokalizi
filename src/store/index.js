22bb651065683fa7623d55eab833b79998ee348c
Pedro Humberto <pedrohumbertogc@gmail.com>
2022-03-22 00:05:28 -0300

    state criado, primeira mutations feita, codigo refatorado

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,33 @@
 import { createStore } from 'vuex'
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+import router from '../router'
 
 export default createStore({
   state: {
+    email: {},
+    password: {}
   },
   getters: {
   },
   mutations: {
+    CADASTRAR_NOVO_USUARIO(state, user) {
+
+      createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+        .then((res) => {
+          console.log("Registrado com Sucesso!")
+          router.push({ name: 'home' })
+        })
+        .catch((error) => {
+          console.log(error.code)
+          alert(error.message)
+        })
+      state.email = user.email
+      state.password = user.password
+
+    }
   },
   actions: {
+
   },
   modules: {
   }
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,52 +1,48 @@
 <template>
-    <div class="container">
-        <h1>Crie sua Conta</h1>
-        <form @onSubmit.prevent="">
-            <input type="email" v-model="email"/>
-            <input type="password" v-model="password" />
-            <button @click.prevent="register">Cadastrar</button>
-        </form>
-            <button @click.prevent="signInWithGoogle">Cadastre-se com Google</button>
-    </div>    
+  <div class="container">
+    <h1>Crie sua Conta</h1>
+    <form @onSubmit.prevent="">
+      <input type="email" v-model="email" />
+      <input type="password" v-model="password" />
+      <button @click.prevent="register">Cadastrar</button>
+    </form>
+    <button @click.prevent="signInWithGoogle">Cadastre-se com Google</button>
+  </div>
 </template>
 
 <script>
-import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 export default {
-    data() {
-        return {
-            email:'',
-            password:''
-        }
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    register() {
+      const email = this.email
+      const password = this.password
+      this.$store.commit('CADASTRAR_NOVO_USUARIO', {email, password})
     },
-    methods: {
-        register(){
-            createUserWithEmailAndPassword(getAuth(), this.email, this.password)
-                .then((data) => {
-                    console.log("Registrado com Sucesso!")
-                    this.$router.push({name: 'home'}) 
-                    
-                })
-                .catch((error) => {
-                    console.log(error.code)
-                    alert(error.message)
-                })
-
-        },
-        signInWithGoogle(){
-            const provider = new GoogleAuthProvider()
-            signInWithPopup(getAuth(), provider)
-                .then((result) => {
-                    console.log(result);
-                    this.$router.push({name: 'home'})
-                }).catch((err) => {
-                    console.log(err)
-                })
-        }
+    signInWithGoogle() {
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(getAuth(), provider)
+        .then((result) => {
+          console.log(result);
+          this.$router.push({ name: "home" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
